9d8122b2536862c4416a6d9bd2d636cf7cef35fe
mohdkaif <mkaif404@gmail.com>
2023-07-11 14:39:39 +0530

    Adding Files

---
+++ src/main/java/com/drizen/chat/chat/ChatController.java
@@ -0,0 +1,26 @@
+package com.drizen.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage chatMessage(@Payload ChatMessage chatMessage){
+
+
+        return chatMessage;
+    }
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+
+        //Add user in websocket session....
+        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+        return chatMessage;
+    }
+}
